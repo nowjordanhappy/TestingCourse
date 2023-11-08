dcea5522b694592a97653e0f1eb9443e917f31f1
Jordan RA <jordan.rojas@devsu.com>
2023-11-08 17:58:25 -0500

    testing

---
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryFake.kt
@@ -0,0 +1,27 @@
+package com.plcoding.testingcourse.core.data
+
+import com.plcoding.testingcourse.core.domain.Product
+import com.plcoding.testingcourse.core.domain.ProductRepository
+import java.lang.Exception
+
+class ProductRepositoryFake: ProductRepository {
+    private val purchasedProducts = mutableListOf<Product>()
+    var shouldReturnError = false
+
+    override suspend fun purchaseProducts(products: List<Product>): Result<Unit> {
+        products.forEach {
+            purchasedProducts.add(it)
+        }
+
+        return if(shouldReturnError){
+            Result.failure(Exception())
+        } else Result.success(Unit)
+    }
+
+    override suspend fun cancelPurchase(purchaseId: Int): Result<Unit> {
+        purchasedProducts.removeIf { it.id == purchaseId }
+        return if(shouldReturnError){
+            Result.failure(Exception())
+        } else Result.success(Unit)
+    }
+}
