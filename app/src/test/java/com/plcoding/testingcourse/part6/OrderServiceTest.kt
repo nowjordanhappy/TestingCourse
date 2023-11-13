f13e9d37140e802d3ef9801a63d3cca7ac5b5f5a
Jordan RA <jordan.rojas@devsu.com>
2023-11-13 09:56:35 -0500

    add test for order service

--- app/src/test/java/com/plcoding/testingcourse/part6/OrderServiceTest.kt
+++ app/src/test/java/com/plcoding/testingcourse/part6/OrderServiceTest.kt
@@ -1,5 +1,53 @@
 package com.plcoding.testingcourse.part6
 
+import com.google.firebase.auth.FirebaseAuth
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-class OrderServiceTest
+class OrderServiceTest{
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var emailClient: EmailClient
+    private lateinit var orderService: OrderService
+
+    @BeforeEach
+    fun setUp(){
+        firebaseAuth = mockk(relaxed = true)
+        emailClient = mockk(relaxed = true)
+
+        orderService = OrderService(
+            auth = firebaseAuth,
+            emailClient = emailClient
+        )
+    }
+
+    @Test
+    fun `Test sending right email and customer for sending email`(){
+        val customerEmail = "jordan@test.com"
+        val productName = "Ice cream"
+
+        every { firebaseAuth.currentUser?.isAnonymous } returns false
+
+        orderService.placeOrder(customerEmail, productName)
+
+        verify {
+            emailClient.send(
+                Email(
+                    subject = "Order Confirmation",
+                    content = "Thank you for your order of $productName.",
+                    recipient = customerEmail
+                )
+            )
+
+            emailClient.send(match {
+                it.subject == "Order Confirmation" &&
+                        it.content == "Thank you for your order of $productName." &&
+                        it.recipient == customerEmail
+            })
+        }
+    }
+}
